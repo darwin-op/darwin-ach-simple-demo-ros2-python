549fcbbc5dfe791541bc748b102df086f156411c
Daniel M. Lofaro <dan@danlofaro.com>
2022-10-10 14:25:43 +0000

    added first test file

---
+++ test.py
@@ -0,0 +1,65 @@
+#/*******************************************************************************
+#* Copyright 2022 Daniel M. Lofaro
+#*
+#* Licensed under the Apache License, Version 2.0 (the "License");
+#* you may not use this file except in compliance with the License.
+#* You may obtain a copy of the License at
+#*
+#*     http://www.apache.org/licenses/LICENSE-2.0
+#*
+#* Unless required by applicable law or agreed to in writing, software
+#* distributed under the License is distributed on an "AS IS" BASIS,
+#* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#* See the License for the specific language governing permissions and
+#* limitations under the License.
+#*******************************************************************************/
+#
+#/* Author: Daniel M. Lofaro */
+
+from time import sleep
+import time
+import rclpy
+
+from std_msgs.msg import String
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node('darwin_simple_demo_ros2_python_publisher')
+    publisher     = node.create_publisher(String, '/darwin/ref/position', 10)
+    publisher_cmd = node.create_publisher(String, '/darwin/cmd', 10)
+
+    msg = String()
+
+    i = 0
+    while rclpy.ok(): 
+      # Stage motors 19, 6, 5, 1, and 2 with units of deg
+      msg.data = 'd 19 20.0 6 20.0 5 20.0 1 20.0 2 -20.0'
+      publisher.publish(msg)
+
+      # Send the staged values to the motors via the ros2ach bridge
+      msg.data = 'post'
+      publisher_cmd.publish(msg)
+
+      # Sleep for 3 seconds
+      sleep(3.0)
+
+      # Stage motors 19, 6, 5, 1, and 2 with units of rad
+      msg.data = 'r 19 -0.35 6 -0.35 5 -0.35 1 -0.35 2 0.35'
+      publisher.publish(msg)
+
+      # Send the staged values to the motors via the ros2ach bridge
+      msg.data = 'post'
+      publisher_cmd.publish(msg)
+
+      # Sleep for 3 seconds
+      sleep(3.0)
+
+
+    # Kill Node
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
